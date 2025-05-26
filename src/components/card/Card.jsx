dee592befeea8478b49677e9dec8020b9d4a29ed
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-05-26 11:38:23 -0500

    feat: enhance Card and User components with overlay and improved styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,14 +6,28 @@ const Card = () => {
   return (
     <article className={c.card}>
       <figure className={c['card__figure']}>
-        <img className={c['card__figure-img']} src="/public/images/image-equilibrium.webp" alt="Equilibrium image" />
+        <img
+          className={c['card__figure-img']}
+          src="/images/image-equilibrium.webp"
+          alt="Equilibrium image"
+        />
+        <div className={c['card__overlay']}>
+          <img
+            className={c['card__overlay-img']}
+            src="/images/icon-view.svg"
+            alt="Eye icon"
+          />
+        </div>
       </figure>
+
       <h1 className={c['card__title']}>Equilibrium #3429</h1>
-      <p className={c['card__paragraph']}>Our Equilibrium collection promotes balance and calm.</p>
+      <p className={c['card__paragraph']}>
+        Our Equilibrium collection promotes balance and calm.
+      </p>
       <Stats />
       <User />
     </article>
   );
-}
+};
 
 export default Card;
--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -4,11 +4,18 @@ const User = () => {
   return (
     <div className={u.user}>
       <figure className={u['user__figure']}>
-        <img className={u['user__figure-img']} src="/public/images/image-avatar.webp" alt="User avatar image" />
+        <img
+          className={u['user__figure-img']}
+          src="/public/images/image-avatar.webp"
+          alt="User avatar image"
+        />
       </figure>
-      <p className={u['user__name']}><span className={u['user__name-creation']}>Creation of</span> Jules Wyvern</p>
+      <p className={u['user__name']}>
+        <span className={u['user__name-creation']}>Creation of</span>{' '}
+        <span className={u['user__name-author']}>Jules Wyvern</span>
+      </p>
     </div>
   );
-}
+};
 
 export default User;
